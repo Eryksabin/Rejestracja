a389adb8c02a0fe1ab0ca5d7dfb97f54465ca8a7
Zoraris <eryk.sabiniewicz@gmail.com>
2024-11-14 11:25:16 +0100

    Done

--- app/src/main/java/com/example/rejestracja/MainActivity.java
+++ app/src/main/java/com/example/rejestracja/MainActivity.java
@@ -1,24 +1,50 @@
 package com.example.rejestracja;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button confirm;
+    private EditText email;
+    private EditText password;
+    private EditText confirmPassword;
+    private TextView changeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        confirm = findViewById(R.id.confirm);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        confirmPassword = findViewById(R.id.confirmPassword);
+        changeText = findViewById(R.id.changeText);
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailText = email.getText().toString().trim();
+                String passwordText = password.getText().toString().trim();
+                String confirmPasswordText = confirmPassword.getText().toString().trim();
+
+                if(emailText.contains("@")) {
+                    if(passwordText.equals(confirmPasswordText)) {
+                        changeText.setText("Witaj " + emailText);
+                    } else {
+                        changeText.setText("Hasła się różnią");
+                    }
+                } else {
+                    changeText.setText("Nieprawidłowy adres e-mail");
+                }
+            }
         });
+
     }
 }
